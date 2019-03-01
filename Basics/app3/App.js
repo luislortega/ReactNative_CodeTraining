3caadb008b7f757dd84ba16e85ce89d0435d5717
Luis Leon <leon.luisgerardo775@gmail.com>
2019-03-01 00:29:54 -0600

    - Studing mutation states

---
+++ Basics/app3/App.js
@@ -0,0 +1,77 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  constructor(){
+    super()
+    
+    this.state = {
+      customStyle: {
+        color:'red'
+      }
+    }
+
+    setInterval(() => {
+      if (this.state.customStyle.color == 'red') {
+        this.setState({
+          customStyle:{
+            color: 'green'
+          }
+        })
+      }else{
+        this.setState({
+          customStyle:{
+            color: 'red'
+          }
+        })
+      }
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.welcome, this.state.customStyle]}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
